544a9a1b302f4a7317a1cea014c9a8d4641e8fe2
Gustavo <gustavoquintero9105@gmail.com>
2022-10-20 21:49:30 -0300

    Updated on 10-20-2022

---
+++ pushgit.sh
@@ -0,0 +1,7 @@
+now=`date +"%m-%d-%Y"`
+
+git add -A
+
+git commit -m "Updated on ${now}"
+
+git push
--- read_data.py
+++ read_data.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+# Datos considerados del 22-03-2020 al 07-09-2022 (900 dias en total)
+
 df = pd.read_excel("dados_full_brasil.xlsx")
 
-print(df)
+initial_date = 25 # 22-03-2020
+total_days = 900
+
+with open("output/data.txt","w") as f:
+    for i in range(initial_date,initial_date + total_days):
+        x = df["new_deaths_smoothed_per_million"][i]
+
+        if pd.isna(x) == True:
+            f.write("%f\n" % 0.0)
+        else:
+            f.write("%f\n" % x)
