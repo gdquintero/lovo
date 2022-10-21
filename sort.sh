48e0eb63a0cd5e88d25baf68534bc80f9b02723b
Gustavo <gustavoquintero9105@gmail.com>
2022-10-21 01:46:31 -0300

    Changes made on 10-21-2022 at 01:46

--- sort.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-gfortran -c -Wall sort.f90
-if [ $? -ne 0 ]; then
-  echo "Compile error."
-  exit
-fi
-#
-echo "Normal end of execution."
--- subset.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-gfortran -c -Wall subset.f90
-if [ $? -ne 0 ]; then
-  echo "Compile error."
-  exit
-fi
-#
-echo "Normal end of execution."
